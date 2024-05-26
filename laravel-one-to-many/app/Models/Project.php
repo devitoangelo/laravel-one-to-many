190fc860be47869f216fd41f645a0e3eb4889879
devitoangelo <angelodevito4@gmail.com>
2024-05-27 00:26:15 +0200

    add method in type and project

--- laravel-one-to-many/app/Models/Project.php
+++ laravel-one-to-many/app/Models/Project.php
@@ -4,9 +4,20 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Project extends Model
 {
     use HasFactory;
-    protected $fillable = ['title', 'slug', 'content', 'cover_image'];
+    protected $fillable = ['title', 'slug', 'content', 'cover_image', 'type_id'];
+
+    /**
+     * Get the user that owns the Project
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function type(): BelongsTo
+    {
+        return $this->belongsTo(Type::class);
+    }
 }
--- laravel-one-to-many/app/Models/Type.php
+++ laravel-one-to-many/app/Models/Type.php
@@ -4,10 +4,26 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
 
 class Type extends Model
 {
     use HasFactory;
 
     protected $fillable = ['name', 'slug'];
+    
+    /**
+     * Get all of the comments for the Type
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function projects(): HasMany
+    {
+        return $this->hasMany(Project::class);
+    }
+
+
+
+
 }
