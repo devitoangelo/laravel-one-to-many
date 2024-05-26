458aa7100c15fc4262daf6cb10b14c74c72f178a
devitoangelo <angelodevito4@gmail.com>
2024-05-26 22:25:51 +0200

    add key to type_id

---
+++ laravel-one-to-many/database/migrations/2024_05_26_201945_add_type_id_foreign_key_to_projects_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('projects', function (Blueprint $table) {
+            
+            $table->unsignedBigInteger('type_id')->nullable()->after('id');
+
+            $table->foreign('type_id')
+            ->references('id')
+            ->on('types')
+            ->onDelete('set null'); //serve per settare il valore null alle categorie che non hanno un type
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('projects', function (Blueprint $table) {
+            $table->dropForeign('projects_type_id_foreign');
+            $table->dropColumn('type_id');
+        });
+    }
+};
